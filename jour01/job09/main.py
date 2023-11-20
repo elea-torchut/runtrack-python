915d99632ac932e824fde4195d32f248d4d2f624
Elea <elea.torchut@laplateforme.io>
2023-11-20 11:36:50 +0100

    job09

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -1,3 +1,24 @@
-nom = 
-prix =
-quantite =
+nom = "Smartphone XYZ"
+prix = 499
+quantite = 50
+
+print(f"Nom : {nom}")
+print(f"Prix : {prix} $")
+print(f"Quantité en stock : {quantite}")
+
+quantite_ajoutee = int(input("Quantité de produits à ajouter en stock : "))
+quantite += quantite_ajoutee
+
+quantite_achetee = int(input("Entrez la quantité de produits que vous souhaitez acheter : "))
+
+if quantite_achetee <= quantite:
+    quantite -= quantite_achetee
+    print(f"Vous avez acheté {quantite_achetee} unités. Nouveau stock : {quantite} unités")
+else:
+    print("Désolé, la quantité demandée n'est pas disponible en stock.")
+
+prix *= 1.10
+
+print(f"Nom : {nom}")
+print(f"Prix : {prix} $")
+print(f"Quantité en stock : {quantite}")
