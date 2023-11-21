8a0f776480cda1410de737807600a3decd7e76a5
Elea <elea.torchut@laplateforme.io>
2023-11-21 15:45:36 +0100

    job07

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -1,6 +1,6 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyz" * 10
 
-for i in range(1, 16):
+for i in range(1, len(alphabet)):
     subset = alphabet[:i*2-1]
     repeated_subset = (subset * 2)[:i*2-1]  # Répéter la chaîne et prendre la même longueur que subset
     print(repeated_subset)
