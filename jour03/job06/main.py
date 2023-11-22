065fbb0c236f1928c06a22184ca91e5462e63c97
Elea <elea.torchut@laplateforme.io>
2023-11-22 11:05:08 +0100

    job06

---
+++ jour03/job06/main.py
@@ -0,0 +1,11 @@
+def Nombre(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0:
+        print("negatif")
+    elif nombre == 0:
+        print("ni positif, ni negatif")
+
+Nombre(6)
+Nombre(-5)
+Nombre(0)
