5fb1611974b94d50795d134073f65979132089cc
Elea <elea.torchut@laplateforme.io>
2023-11-22 12:16:35 +0100

    pour aller plus loin

---
+++ jour03/job_pour_aller_plus_loin/main.py
@@ -0,0 +1,12 @@
+def inverser_chaine(chaine):
+    chaine_inverse = chaine[::-1]
+    return chaine_inverse
+
+resultat = inverser_chaine("nikana")
+print(resultat)
+resultat = inverser_chaine("aele")
+print(resultat)
+resultat = inverser_chaine("latsyrc")
+print(resultat)
+resultat = inverser_chaine("divad")
+print(resultat)
