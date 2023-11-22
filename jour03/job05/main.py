5b17fbb732729f12c824f9227045d54e36d2f57c
Elea <elea.torchut@laplateforme.io>
2023-11-22 10:59:18 +0100

    job05

---
+++ jour03/job05/main.py
@@ -0,0 +1,15 @@
+def calcule(num1,operator,num2):
+    if operator == '+':
+        print( num1 + num2 )
+    elif operator == '-':
+        print( num1 - num2 )
+    elif operator == '*':
+        print( num1 * num2 )
+    elif operator == '/':
+        print( num1 / num2 )
+    
+
+calcule(5,'+',4)
+calcule(7,'*',2)
+calcule(6,'/',3)
+calcule(10,'-',5)
