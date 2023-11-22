cf1c55e84e0474111e99fbbb71301c6c0b98068d
Elea <elea.torchut@laplateforme.io>
2023-11-22 11:12:44 +0100

    job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,17 @@
+def Langage(langage):
+    if langage.lower() == "javascript":
+        print("tu es un développer web")
+    elif langage.lower() == "python":
+        print("tu es un développer IA")
+    elif langage.lower() == "java":
+        print("tu es un développer logiciel")
+    elif langage.lower() == "reactjs":
+        print("tu es un développer mobile")
+    else:
+        print("un jour, je serai le meilleur développer...")
+
+Langage("JavaScript")
+Langage("python")
+Langage("java")
+Langage("reactjs")
+Langage("dev")
