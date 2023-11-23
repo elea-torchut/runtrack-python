ca5a70024e4169e721785cf9f8e1f40ad882b4b7
Elea <elea.torchut@laplateforme.io>
2023-11-23 13:47:08 +0100

    job10

---
+++ jour04/job10/main.py
@@ -0,0 +1,8 @@
+L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+produit = 1
+
+for nombre in L:
+    if 90 <= nombre >= 25:
+        produit *= nombre
+
+print(f"le produit de toutes les valeurs de la liste comprises dans l'intervalle [25,90] est : {produit}")
