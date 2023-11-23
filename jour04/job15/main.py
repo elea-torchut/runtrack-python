bcf13bf8916b12d5cbe4e6d787d2cfd8f95376df
Elea <elea.torchut@laplateforme.io>
2023-11-23 16:15:34 +0100

    job15

---
+++ jour04/job15/main.py
@@ -0,0 +1,23 @@
+def tri_a_bulles(liste):
+    n = 0
+
+    for _ in liste:
+        n += 1
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if liste[j] > liste[j+1]:
+                liste[j], liste[j+1] = liste[j+1], liste[j]
+
+def arrondir_nombres(liste):
+    for i in range(len(liste)):
+        liste[i] = int(liste[i] + 0.5)
+
+liste_nombres = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
+print("Avant arrondi et tri :", liste_nombres)
+
+arrondir_nombres(liste_nombres)
+
+tri_a_bulles(liste_nombres)
+
+print("Après arrondi et tri :", liste_nombres)
